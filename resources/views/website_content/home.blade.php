83a2eeca5caaed4df774718432fee2cf93be8cb3
Faraz ur rehman <shariffaraz@gmail.com>
2018-08-06 10:54:26 +0500

    Fixed main page responsive structure, added About us

--- resources/views/website_content/home.blade.php
+++ resources/views/website_content/home.blade.php
@@ -1,18 +1,26 @@
 @extends('website_content.custom_layouts.app')
 @section('content')
-@section('css')
-@endsection
-	<!-- <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark"> -->
-  <div class="home-slider">
-    <div class="slider-text">
-      <h1 class="display-4 top_title_slider">Magic Internet Money <span style="color: #ffffff">-</span></h1>
-      <h1 class="display-4 top_title_slider_second">You Can't Go Wrong<br>With The Classics.</h1>
-      <p class="lead my-3 lead_text_slider">So you want to buy Bitcoin. Bitcoins are one of many trendy <br> cryprocurrency sold in the internet today...</p>
-      <br>
-      <a href="#" class="view_more_class">VIEW MORE</a>
-      <a href="#" class="signup_btn_class">SIGNUP</a>
+<section class="engine"><a href="https://mobiri.se/i">free site creation software</a></section><section class="cid-qTkA127IK8 mbr-fullscreen mbr-parallax-background" id="header2-1">
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-4 mbr-fonts-style display-2"><p>
+                <div style="text-align: left; float: right; width: 53%">Magic Internet Money - </div></p>
+                <div style="clear: both"></div>
+                <p><div style="text-align: left; float: right; color: white; width: 53%">You Can't Go Wrong <br/> With The Classics.</div></p></h1>
+                <div style="clear: both"></div>
+                <p class="mbr-text pb-3 mbr-fonts-style display-6">
+                    <div style="text-align: left; float: right; width: 53%">So you want to buy Bitcoins. Bitcoins are one of many trendy<br>Cryptocurrencies sold on the internet today...</div></p>
+                    <div style="clear: both; padding-bottom: 20px"></div>
+                    <div class="mbr-section-btn" style="width: 55%; float: right; text-align: left"><a class="btn btn-md btn-white display-4" href="https://mobirise.com">View More</a>
+                    <a class="btn btn-md btn-white display-4" href="https://mobirise.com">Signup</a></div>
+            </div>
+        </div>
     </div>
-  </div>
-@section('js')
-@endsection
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
 @endsection
